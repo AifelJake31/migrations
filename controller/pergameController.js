a9c8eccafd2f5e0f502b96fba6c5fa0e71278883
MSI\aifel <aifel.regunay@luckycue.tech>
2023-08-25 21:29:27 +0800

    created function that will get all the data's from pick one bets

--- controller/pergameController.js
+++ controller/pergameController.js
@@ -3,10 +3,38 @@ import Sharks from "../models/shark.js";
 import RackBets from "../models/rack_bet.js"
 import Racks from "../models/rack.js"
 import Bettor_Wallet from "../models/bettor_wallet.js"
-import Pick_One_Bets from "../models/pick_one_bet.js";
-import Bettors from '../models/bettor.js'
-import Pick_Ones from '../models/pick_one.js';
-import Event_Sharks from "../models/event_shark.js"
+import Pick_One_Bet from "../models/pick_one_bet.js";
+import Bettor from '../models/bettor.js'
+import Pick_One from '../models/pick_one.js';
+import Event_Shark from "../models/event_shark.js"
+import "./../models/index.js"
+
+// import db from "../models/index.js"
+
+// async function getAllDataFromDatabase() {
+//     try {
+//       const pickOneBets = await db.Pick_One_Bet.findAll({
+//         include: [
+//           { model: db.Bettor, as: 'bettor' },
+//           { model: db.Pick_One, as: 'pick_one' },
+//           { model: db.Event_Shark, as: 'event_shark' },
+//         ],
+//       });
+  
+//       return pickOneBets;
+//     } catch (error) {
+//       throw new Error('Error fetching data from the database: ' + error.message);
+//     }
+//   }
+  
+//   // Usage example:
+//   getAllDataFromDatabase()
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
 
 const getAllSharks = asyncHandler(async (req, res) => {
     try {
@@ -51,38 +79,37 @@ const getAllBets = asyncHandler(async(req, res) => {
     }
 })
 
-const getAllPickOneBets = asyncHandler(async(req, res) => {
+const getAllPickOneBets = asyncHandler(async (req, res) => {
     try {
-        const pickOneBets = await Pick_One_Bets.findAll({
-          include: [
-            {
-              model: Bettors,
-              as: 'bettor',
-            },
-            {
-              model: Pick_Ones,
-              as: 'pick_ones',
-            },
-            {
-              model: Event_Sharks,
-              as: 'event_sharks',
-            },
-          ],
-        });
-    
-        return pickOneBets;
+        const pickOneBet = await Pick_One_Bet.findAll({
+        include: [
+          {
+            model: Bettor, 
+            as: 'bettor',
+          },
+          {
+            model: Pick_One, 
+            as: 'pick_one',
+          },
+          {
+            model: Event_Shark, 
+            as: 'event_shark',
+          },
+        ],
+      });
+  
+      return res.status(200).json({ success: true, data: pickOneBet });
+    } catch (error) {
+      if (error.name === 'SequelizeValidationError') {
+        const errorMessages = error.errors.map((validationError) => validationError.message);
+        return res.status(400).json({ success: false, error: errorMessages });
+      } else {
+        console.error(error);
+        return res.status(500).json({ success: false, error: 'Server Error' });
       }
-
-      catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errorMessages = error.errors.map((validationError) => validationError.message);
-            res.status(400).json({ success: false, error: errorMessages });
-        } else {
-            console.error(error);
-            res.status(500).json({ success: false, error: 'Server Error' });
-        }
     }
-})
+  });
+  
 
 
 
