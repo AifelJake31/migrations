961da73a67f29cc67d873f3bd688f8b916085097
MSI\aifel <aifel.regunay@luckycue.tech>
2023-08-25 21:30:39 +0800

    added model associations

--- models/index.js
+++ models/index.js
@@ -1,57 +1,27 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+import Pick_One from './pick_one.js';
+import Bettor from './bettor.js';
+import Pick_One_Bet from './pick_one_bet.js'
+import Event_Shark from './event_shark.js';
 
-import Pick_One from './pick_one';
-import Bettor from './bettor';
-import Pick_One_Bet from './pick_one_bet'
-
-Pick_One.hasMany(models.Pick_One_event, {
+Pick_One.hasMany(Pick_One_Bet, {
   foreignKey: "pick_one_id",
-  constraints: true
+  constraints: true,
+  as: 'pick_one_events'
 })
-Bettor.hasMany(models.Pick_One_Bet, { 
+Pick_One_Bet.belongsTo(Bettor, {
+  foreignKey: 'bettor_id',
+  as: 'bettor',
+});
+Pick_One_Bet.belongsTo(Pick_One, {
+  foreignKey: 'pick_one_id',
+  as: 'pick_one',
+});
+Pick_One_Bet.belongsTo(Event_Shark, {
+  foreignKey: 'event_shark_id',
+  as: 'event_shark',
+});
+Bettor.hasMany(Pick_One_Bet, { 
   foreignKey: 'bettor_id', 
-  as: 'pickOneBets' 
+  as: 'pick_one_bets' 
 });
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-
-
-module.exports = db;
